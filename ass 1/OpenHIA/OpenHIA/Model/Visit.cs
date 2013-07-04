a6db681ee514231ed19cd7aa01672dbec714123e
TanThinh <ngotanthinh92@gmail.com>
2013-07-04 12:01:19 -0700

    Add Visit

--- ass 1/OpenHIA/OpenHIA/Model/Visit.cs
+++ ass 1/OpenHIA/OpenHIA/Model/Visit.cs
@@ -8,5 +8,13 @@ namespace OpenHIA.Model
 {
     public class Visit
     {
+        /// <summary>
+        /// Getters and Setters for properties
+        /// </summary>
+        public DateTime MyProperty { get; set; }
+        public string Place { get; set; }
+        public Doctors TreatingDoctor { get; set; }
+        public string Diagnosis { get; set; }
+        public string Outcome { get; set; }
     }
 }
--- ass 1/OpenHIA/OpenHIA/Program/Program.cs
+++ ass 1/OpenHIA/OpenHIA/Program/Program.cs
@@ -10,7 +10,7 @@ namespace OpenHIA.Program
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Program starts");
+            Console.WriteLine("Program starts ");
             Console.Read();
         }
     }
